76c165d94a383c5fec9920f51fd54db74cbc77f4
Franziskus Kiefer <franziskuskiefer@gmail.com>
2013-07-15 23:41:21 +0100

    content and style

---
+++ javascripts/custom.js
@@ -0,0 +1,22 @@
+function UnCryptMailto( s )
+    {
+        var n = 0;
+        var r = "";
+        for( var i = 0; i < s.length; i++)
+        {
+            n = s.charCodeAt( i );
+            if( n >= 8364 )
+            {
+                n = 128;
+            }
+            r += String.fromCharCode( n - 1 );
+        }
+        return r;
+    }
+
+    function linkTo_UnCryptMailto( s )
+    {
+	var mail = UnCryptMailto( s );
+        location.href=mail;
+//	alert("You can contact me at "+mail);
+    }
